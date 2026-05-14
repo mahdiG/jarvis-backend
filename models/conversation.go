773b5b97068d4bde8a2f750759361709ed76fb78
Mahdi Gerami <mahdi.gerami77@gmail.com>
2026-05-14 22:13:17 +0330

    merge conversation and message model files

--- models/conversation.go
+++ models/conversation.go
@@ -4,3 +4,10 @@ type Conversation struct {
 	Base
 	Title string `gorm:"not null" validate:"required"`
 }
+
+type Message struct {
+	Base
+	ConversationID UID    `gorm:"not null;index" validate:"required"`
+	Role           string `gorm:"not null" validate:"required"`
+	Content        string `gorm:"not null" validate:"required"`
+}
--- models/message.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Message struct {
-	Base
-	ConversationID UID    `gorm:"not null;index" validate:"required"`
-	Role           string `gorm:"not null" validate:"required"`
-	Content        string `gorm:"not null" validate:"required"`
-}
